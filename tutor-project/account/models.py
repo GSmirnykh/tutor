687fa7c3f85662c584be3810c60a6b432f718f57
Sergey Ilin <ilin_sergey90@mail.ru>
2023-01-14 19:35:31 +0300

    Небольшие изменения

--- tutor-project/account/models.py
+++ tutor-project/account/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Cours(models.Model):
     title = models.CharField('Название курса', max_length=50)
     description = models.TextField('Описание курса')
-    lenght = models.IntegerField('Продолжительность курса',)
+    lenght = models.IntegerField('Продолжительность курса')
     cost_individual = models.IntegerField('Стоимость индивидульного занятия')
     cost_group = models.IntegerField('Стоимость группового занятия')
 
@@ -17,12 +17,16 @@ class Cours(models.Model):
 
 
 class Pupil(models.Model):
+    CHOISES = [
+        ('individual', 'индивидуальное'),
+        ('group', 'групповое')
+    ]
     name = models.CharField('Имя', max_length=30)
     surname = models.CharField('Фамилия', max_length=30)
     parents = models.TextField('Представитель')
     phone = models.CharField('Контактный телефон', max_length=10)
-    is_group = models.BooleanField('Групповые занятия', default=False)
-    is_individual = models.BooleanField('Индивидуальные занятия', default=False)
+    education_form = models.CharField('Форма обучения', max_length=50, choices=CHOISES, default='individual')
+
 
     def __str__(self):
         pupil = self.name + ' ' + self.surname
